ad76b0ef229379da598541eadcd1fadc8ddfde14
WalidAssknid <assknid.walid01@gmail.com>
2026-03-04 17:48:04 +0100

    Implement Volley networking and update SecondActivity UI
    - Add Volley dependency to `app/build.gradle` and enable Internet permissions and cleartext traffic in `AndroidManifest.xml`.
    - Initialize Volley `RequestQueue` in `SecondActivity` to perform GET and POST requests to the SmartHouse API.
    - Add `requestSuccesListener` and `requestErrorListener` methods to handle API responses and logging.
    - Update `createDeviceView` to include a `Button` that displays the device's ON/OFF status.

--- app/src/main/java/com/example/smarthouse/SecondActivity.java
+++ app/src/main/java/com/example/smarthouse/SecondActivity.java
@@ -3,16 +3,27 @@ package com.example.smarthouse;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -28,7 +39,54 @@ public class SecondActivity extends AppCompatActivity {
         });
         Intent intent = getIntent();
         LinearLayout linearlayout = (LinearLayout) findViewById(R.id.linearlayout);
+        String url = "http://happyresto.enseeiht.fr/smartHouse/api/v1/";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                requestSuccesListener(), requestErrorListener());
+        queue.add(jsonObjectRequest);
+
+        StringRequest sr = new StringRequest(Request.Method.POST, url,
+                requestSuccesListener(), requestErrorListener()) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("deviceId", String.valueOf(deviceId));
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        queue.add(sr);
+
+
     }
+
+    public static final String TAG = "MyTag";
+
+    private Response.Listener<JSONObject> requestSuccesListener() {
+        return new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+            }
+        };
+    }
+
+
+    private Response.ErrorListener requestErrorListener() {
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, error.getMessage());
+            }
+        };
+    }
+
 
     public View createDeviceView(String deviceName, Boolean deviceStatus) {
         RelativeLayout layout = new RelativeLayout(this);
@@ -41,7 +99,12 @@ public class SecondActivity extends AppCompatActivity {
         TextView someTextView = new TextView(this);
         someTextView.setText(deviceName);
         someTextView.setTextSize(20);
+        Button button = new Button(this);
+        button.setText(deviceStatus ? "ON" : "OFF");
+        button.setTextSize(20);
+
         layout.addView(someTextView, params);
+        layout.addView(button, params);
         return layout;
     }
 }
